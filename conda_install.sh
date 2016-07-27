a750f377dd0a9d5b1e85963f61e16daa23f89174
PowChow <pauline.chow@gmail.com>
2016-07-27 14:22:50 -0700

    added source bashrc

--- conda_install.sh
+++ conda_install.sh
@@ -5,3 +5,4 @@ wget http://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh
 bash Anaconda2-4.1.1-Linux-x86_64.sh -b
 echo 'PATH="/home/ubuntu/anaconda2/bin:$PATH"' >> .bashrc
 . .bashrc
+source ~/.bashrc
